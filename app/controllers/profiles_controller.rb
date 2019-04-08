c9937682be175efe0dc163af884cf68c7335cd35
Wesley Haman <wesley_haman@live.nl>
2019-04-08 13:02:17 +0000

    Added user profiles form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,6 +2,7 @@ class ProfilesController < ApplicationController
     
   # GET to /users/:user_id/profilenew
   def new
-    # Render blank profile details form    
+    # Render blank profile details form
+    @profile = Profile.new
   end
 end
